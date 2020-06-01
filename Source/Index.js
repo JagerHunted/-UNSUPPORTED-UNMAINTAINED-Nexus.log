b6bb6765a96c59e1dbced2b81b394dddcea5ab76
JagerHunted <theta.developer@gmail.com>
2020-05-31 21:46:19 -0500

    Create Full-Logging System full with crash saving.

--- Source/Index.js
+++ Source/Index.js
@@ -6,3 +6,56 @@
     Logger.js  —  Index.js
 
 */
+
+
+const Colors = require('colors');
+const Time = require('time-stamp');
+const File = require('fs')
+
+
+class Log {
+
+
+    constructor(Message) {
+
+        this.Message = Message
+
+    }
+
+
+
+    Info(Message) {
+
+        console.log("[".green + Time("HH:MM:ss").green + "] [Info] ".green + Message.green)
+        let String = "[" + Time("HH:MM:ss") + "] [Info] " + Message
+        File.appendFileSync('./Logs/' + Time("YY-MM-DD") + '.log', '\n' + String)
+
+    }
+
+
+
+    Warn(Message) {
+
+        console.log("[".yellow + Time("HH:MM:ss").yellow + "] [Warn] ".yellow + Message.yellow)
+        let String = "[" + Time("HH:MM:ss") + "] [Warn] " + Message
+        File.appendFileSync('./Logs/' + Time("YY-MM-DD") + '.log', '\n' + String)
+
+    }
+
+
+
+    Error(Message) {
+
+        console.log("[".red + Time("HH:MM:ss").red + "] [Error] ".red + Message.red)
+        let String = "[" + Time("HH:MM:ss") + "] [Error] " + Message
+        File.appendFileSync('./Logs/' + Time("YY-MM-DD") + '.log', '\n' + String)
+
+    }
+
+
+
+}
+
+
+
+module.exports = Log;
--- Source/Test.js
+++ Source/Test.js
@@ -6,3 +6,12 @@
     Logger.js  —  Test.js
 
 */
+
+
+const Nexus = require('./Index.js')
+const Log = new Nexus;
+
+
+Log.Info("One!")
+Log.Warn("Two!")
+Log.Error("Three!")
